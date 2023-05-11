f73d4fb56069de0c0045a22e71c0c18951d0be8f
Edme <erikdanielme@outlook.com>
2023-05-11 14:20:11 -0600

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EquiposComponent } from './components/equipos/equipos.component';
+import { IndexComponent } from './components/index/index.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquiposComponent,
+    IndexComponent,
+    NavigationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
